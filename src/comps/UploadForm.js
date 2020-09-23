647decf8c296dd5239a836bfb0fb70459bad1c3c
tnewman <tnewman@meditech.com>
2020-09-23 17:19:46 -0400

    node updated

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const UploadForm = () => {
 
+const changeHandler = (e) => {
+    console.log("changed");
+}
+
     return (
         <form>
-        <input type="file" />
+        <input type="file" onChange={ changeHandler} />
         </form>
     )
 }
