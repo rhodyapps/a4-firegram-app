3640327b5394d561649562a8e8fb25c32b40c6a1
tnewman <tnewman@meditech.com>
2020-09-24 12:42:32 -0400

    modal image click

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>RWU Comsc230 Assignment 4 & Group Project 1</h1>
+      <h2>Firestore Image Gallery</h2>
+      <p>World Famous Pumpkin Photos</p>
     </div>
   )
 }
