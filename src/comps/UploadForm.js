01f28eb7760a54ded75f3eaec45c2268c6fc25db
tnewman <tnewman@meditech.com>
2020-09-23 22:18:25 -0400

    file selection handler

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -3,7 +3,9 @@ import React from 'react';
 const UploadForm = () => {
 
 const changeHandler = (e) => {
-    console.log("changed");
+   // show that the form is working console.log("changed"); then remove this line and add the following
+   let selected = e.target.files[0];
+   console.log(selected); // this will display the metadata for the selected file
 }
 
     return (
