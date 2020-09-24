a4d41a6b659e08de45ba35bd17672ef52005f77e
tnewman <tnewman@meditech.com>
2020-09-24 05:33:48 -0400

    display output

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -2,6 +2,7 @@ import React, { useState} from 'react';
 
 const UploadForm = () => {
     const [file, setFile] = useState(null);
+    const [error, setError] = useState(null); // error handler hook
 
     const types =['image/png', 'image/jpeg'];
 
@@ -10,15 +11,21 @@ const changeHandler = (e) => {
    let selected = e.target.files[0];
    if (selected && types.includes(selected.type)) {
        setFile(selected);
+       setError(''); // reset error flag
        console.log(selected); // this will display the metadata for the selected file
    } else {
-       setFile(null);
+       setFile(null); // if they don't choose a png or jpeg we set the state to null
+       setError('Please select an image file (png or jpeg)');
    }
 }
 
     return (
         <form>
         <input type="file" onChange={ changeHandler} />
+        <div className="output">
+{ error && <div className="error">{ error }</div> }
+{ file && <div>{ file.name }</div> }
+        </div>
         </form>
     )
 }
