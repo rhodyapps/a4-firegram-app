cfb4ac560bb16a093796ec783508eeb730b0215c
tnewman <tnewman@meditech.com>
2020-09-24 08:02:26 -0400

    image url in db

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,7 +18,8 @@ import 'firebase/firestore';
 
   const projectStorage = firebase.storage();
   const projectFirestore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
   
-  export { projectStorage, projectFirestore };
+  export { projectStorage, projectFirestore, timestamp };
 
  
