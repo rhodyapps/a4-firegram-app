1b11ca6346d5da3d3b5f0ff7affa485d2fafd7f7
tnewman <tnewman@meditech.com>
2020-09-24 05:12:07 -0400

    select file

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -3,13 +3,16 @@ import React, { useState} from 'react';
 const UploadForm = () => {
     const [file, setFile] = useState(null);
 
+    const types =['image/png', 'image/jpeg'];
 
 const changeHandler = (e) => {
    // show that the form is working console.log("changed"); then remove this line and add the following
    let selected = e.target.files[0];
-   if (selected) {
+   if (selected && types.includes(selected.type)) {
        setFile(selected);
        console.log(selected); // this will display the metadata for the selected file
+   } else {
+       setFile(null);
    }
 }
 
