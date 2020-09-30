2b582aef7b95fe73bd21136d19ecfb2034f16ee3
rhodyapps <rhodyapps@gmail.com>
2020-09-30 07:48:57 -0400

    update storage

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -11,7 +11,7 @@ const useStorage = (file) => {
 // references
 
 const storageRef = projectStorage.ref(file.name);
-const collectionRef = projectFirestore.collection('guitars');
+const collectionRef = projectFirestore.collection('images');
 
 storageRef.put(file).on('state_changed', (snap) => {
     let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
