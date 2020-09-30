cf3cce1149d9c244f9a4acd816c4d3d35f9cadae
rhodyapps <rhodyapps@gmail.com>
2020-09-30 08:04:06 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,15 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 // Your web app's Firebase configuration
+  // Your web app's Firebase configuration - Use the config settings from your firebase project
   var firebaseConfig = {
-    apiKey: "AIzaSyDiOgpcCkuN1ObDV1zJSk5PlQjBBTs4jE4",
-    authDomain: "a4-firegram.firebaseapp.com",
-    databaseURL: "https://a4-firegram.firebaseio.com",
-    projectId: "a4-firegram",
-    storageBucket: "a4-firegram.appspot.com",
-    messagingSenderId: "777873835334",
-    appId: "1:777873835334:web:a1b10483c6fe1f381cd147"
+    apiKey: "********************",
+    authDomain: "*******************",
+    databaseURL: "*******************",
+    projectId: "********************",
+    storageBucket: "******************",
+    messagingSenderId: "*****************",
+    appId: "*************************"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
